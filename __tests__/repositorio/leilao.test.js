755c3314a4c1371f7d4da02e98969a9a2b1c849a
Gustavo Gama <gustavogama494@gmail.com>
2024-04-04 10:54:45 -0300

    modulo 3 aula 2

--- __tests__/repositorio/leilao.test.js
+++ __tests__/repositorio/leilao.test.js
@@ -1,12 +1,46 @@
 import { obtemLeiloes } from "../../src/repositorio/leilao"
+import apiLeiloes from '../../src/servicos/apiLeiloes';
 
-jest.mock('../../src/servicos/apiLeiloes')
+jest.mock('../../src/servicos/apiLeiloes');
+
+const mockLeiloes = [
+  {
+    id: 1,
+    nome: 'Leilão',
+    descricao: 'Descrição do leilão'
+  }
+];
+
+const mockRequisicao = (retorno) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data: retorno
+      })
+    }, 200);
+  });
+}
+
+const mockRequisicaoErro = () => {
+return new Promise((_,reject) => {
+    setTimeout(() => {
+    reject();
+    }, 200);
+});
+}
 
 describe('repositorio/leilao',() =>{
     describe('obtemLeiloes', () => {
       test('deve retornar uma lista de leilões',async () => { 
+        apiLeiloes.get.mockImplementation(() => mockRequisicao(mockLeiloes))
         const leiloes = await obtemLeiloes();
-        console.log(leiloes)
+        expect(leiloes).toEqual(mockLeiloes);
+      });
+      
+      test('deve retornar uma lista vazia quando a requisição falhar',async () => { 
+        apiLeiloes.get.mockImplementation(() => mockRequisicaoErro(mockLeiloes))
+        const leiloes = await obtemLeiloes();
+        expect(leiloes).toEqual([]);
       })  
     })
 })
